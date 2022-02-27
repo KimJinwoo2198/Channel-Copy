06f565aae55eb9cf66eb92eb25bcf8dda711193f
BattleBot <admin@battlebot.kr>
2022-02-27 12:25:29 +0900

    Add main Source

---
+++ Channel.py
@@ -0,0 +1,33 @@
+#from unicodedata import name
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!복제'):
+        if message.author.guild_permissions.manage_messages:
+            new = await message.channel.clone()
+            nposition = message.channel.position
+            await new.edit(position=nposition)
+            embed = discord.Embed(title='채널 복제 완료', color=0x2f3136)
+            embed.add_field(name='**채널 복제 명령어 사용자**',value=message.author.mention, inline=False)
+            embed.add_field(name='**채널 이름**',value=new.name,inline=False)
+            #embed.set_image(url='https://media.giphy.com/media/HhTXt43pk1I1W/giphy.gif')
+            await new.send(embed=embed)
+            await new.send(message.author.mention)
+
+    if message.content.startswith('!재생성'):
+        if message.author.guild_permissions.manage_messages:
+            new = await message.channel.clone()
+            nposition = message.channel.position
+            await new.edit(position=nposition)
+            await message.channel.delete()
+            embed = discord.Embed(title='채널 재생성 완료', color=0x2f3136)
+            embed.add_field(name='**채널 재생성 명령어 사용자**',value=message.author.mention, inline=False)
+            embed.add_field(name='**채널 이름**',value=new.name,inline=False)
+            #embed.set_image(url='https://media.giphy.com/media/HhTXt43pk1I1W/giphy.gif')
+            await new.send(embed=embed)
+            await new.send(message.author.mention)
+
+client.run('OTMyNTg1NDE3NjYyOTM1MDUw.YeVH2g.k51eLNFzQ6lzYJSYXuFg-Rnv8cI')
